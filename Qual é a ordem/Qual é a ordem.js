e7491d9f6e548cc15d0a466c7d069886c75dd349
LukEscopeta <lfcnarcizo73@gmail.com>
2025-02-24 21:56:25 -0300

    lacos

---
+++ Qual é a ordem/Qual é a ordem.js
@@ -0,0 +1,5 @@
+    for (var activity of ['dancing', 'jumping', 'singing']) {
+        for (var family of ['brother', 'mother', 'father']) {
+            console.log(activity + ' ' + family);
+        }
+    }
